8a55cd33ade72dd08244caaf2acd29116f034256
Marc Brackett <admin@bookvetter.com>
2017-04-06 18:19:01 -0400

    trying

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,15 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.create(dose_params)
+    redirect_to dose_path(@dose)
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to doses_path
+  end
 end
